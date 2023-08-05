5df813c610d2384473123808d9f761f6d7465ede
glpat-wFDyyJXhUCjdaGzFW1ZY <glpat-wFDyyJXhUCjdaGzFW1ZY>
2023-08-05 18:18:47 +0100

    fix & 15 & 50

--- 6. Zig-zag conversation/Zig-zagConversation.ts
+++ 6. Zig-zag conversation/Zig-zagConversation.ts
@@ -1,32 +1,36 @@
-s = "PAYPALISHIRING";
-numRows = 3;
-
-const zigZag = (s, numRows) => {
-    if( numRows <= 1 ){
+function zigZag(s: string, numRows: number): string {
+  if (numRows <= 1) {
     return s;
-}
-let i, row = 0;
-let down = true;
-let array = [];
-for( i = 0; i < numRows; i++ ){
+  }
+
+  let i,
+    row = 0;
+  let down = true;
+  let array: string[] = [];
+  for (i = 0; i < numRows; i++) {
     array[i] = "";
-}
-for( i = 0; i < s.length; i++ ){
+  }
+
+  for (i = 0; i < s.length; i++) {
     array[row] += s[i];
-    if( row == 0 ){
-        down = true;
+    if (row === 0) {
+      down = true;
     }
-    if( row == numRows - 1 ){
-        down = false;
+    if (row === numRows - 1) {
+      down = false;
     }
     down ? row++ : row--;
-}
-var answer = "";
-for( i = 0; i < numRows; i++ ){
+  }
+
+  let answer = "";
+  for (i = 0; i < numRows; i++) {
     answer += array[i];
-}
-console.log(answer);
-return answer;
+  }
+
+  console.log(answer);
+  return answer;
 }
 
+const s = "PAYPALISHIRING";
+const numRows = 3;
 zigZag(s, numRows);
