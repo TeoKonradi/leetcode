3466cf1bed8dee90eb0e7d7330be3d89e4402c2b
glpat-wFDyyJXhUCjdaGzFW1ZY <glpat-wFDyyJXhUCjdaGzFW1ZY>
2023-08-05 18:18:22 +0100

    fix & 15 & 50

--- 1. Two sum/1.ts
+++ 1. Two sum/1.ts
@@ -1,4 +1,4 @@
-function twoSum(nums: number[], target: number): number[] {
+function twoSum(nums: number[], target: number): number[] | undefined {
   for (let i: number = 0; i < nums.length; i++) {
     console.log(i);
     for (let j: number = i + 1; j < nums.length; j++) {
