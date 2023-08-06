cc8484bffe9b258d18fec359df6cecc4be719a32
glpat-wFDyyJXhUCjdaGzFW1ZY <glpat-wFDyyJXhUCjdaGzFW1ZY>
2023-08-06 10:49:23 +0100

    2 & 3

---
+++ 2. Add two numbers/2.ts
@@ -0,0 +1,41 @@
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+  let dummyHead = new ListNode(0);
+  let current = dummyHead;
+  let carry = 0;
+
+  while (l1 !== null || l2 !== null) {
+    const val1 = l1 ? l1.val : 0;
+    const val2 = l2 ? l2.val : 0;
+
+    const sum = val1 + val2 + carry;
+    carry = Math.floor(sum / 10);
+    current.next = new ListNode(sum % 10);
+
+    current = current.next;
+    if (l1) l1 = l1.next;
+    if (l2) l2 = l2.next;
+  }
+
+  if (carry > 0) {
+    current.next = new ListNode(carry);
+  }
+
+  return dummyHead.next;
+}
+
+// Example usage:
+const list1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+const list2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+
+const result = addTwoNumbers(list1, list2);
+
+// Output: The result variable will hold the linked list representing the sum (e.g., 7 -> 0 -> 8)
